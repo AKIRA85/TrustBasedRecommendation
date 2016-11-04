f77e19541fcfdb8fafc8bd70ce5cef1c7fd4c2a7
AKIRA85 <akshay.cs132iitp.ac.in>
2016-11-04 23:05:35 +0530

    implemented hybrid recommendation and tested on movielens dataset

--- movie_collab.py
+++ movie_collab.py
@@ -1,15 +1,14 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 import numpy as np
 import networkx as nx
 import json
 import math
 
-def calculate_trust(row, max_freq):
-	sigma = 0.5;
-	return sigma*row['mean']+((1-sigma)*row['count'])/max_freq
+number_of_thresholds = 11
 
-def logic_function(x):
-	return 1/(1+math.exp(-x))
+def getThreshold(t):
+	return (t*1.0)/(number_of_thresholds-1)
 
 def parse(path): 
 	f = open(path)
@@ -36,19 +35,21 @@ def calcuate_similarity(pivot_table, user_data, product_data, i, j):
 
 #create pandas dataframe
 
-# df = pd.read_csv('ratings_Electronics.csv', header=None, names=['reviewerID', 'asin', 'overall', 'unixReviewTime'])
-df = getDF('Digital_Music_5.json')
-# #df = getDF('test_5500.json')
-df.drop(['reviewerName', 'helpful', 'reviewText', 'reviewTime', 'summary'], inplace=True, axis=1)
+
+df = pd.read_csv('ml-1m/ratings.dat', header=None, names=['reviewerID', 'movieID', 'overall', 'unixReviewTime'], sep=':+', engine='python')
+# df = getDF('Digital_Music_5.json')
+#df = getDF('test_5500.json')
+# df.drop(['reviewerName', 'helpful', 'reviewText', 'reviewTime', 'summary'], inplace=True, axis=1)
 df.sort_values('unixReviewTime')
 
 #create product data
-product_data = pd.DataFrame(df.groupby('asin')['overall'].agg([np.mean, np.std, 'count'])).fillna(1)
+product_data = pd.DataFrame(df.groupby('movieID')['overall'].agg([np.mean, np.std, 'count'])).fillna(1)
 
-# top_product = product_data.sort_values('count').tail(20).index.values
-# df = df[df.asin.isin(top_product)]
+top_product = product_data.sort_values('count').tail(100).index.values
+df = df[df.movieID.isin(top_product)]
+print len(df)
 
-split_time = 1245000000
+split_time = df['unixReviewTime'].quantile([.75])[0.75]
 
 after = df[df.unixReviewTime>split_time]
 before = df[df.unixReviewTime<=split_time]
@@ -62,14 +63,17 @@ before = before[before.reviewerID.isin(common_users)]
 
 #create user data
 user_data = pd.DataFrame(before.groupby('reviewerID')['overall'].agg([np.mean, np.std, 'count'])).fillna(1)
+print "user_data"
+print user_data.describe();
 user_data = user_data[user_data['count'] > 4]
 accepted_users = user_data.index.values
+print len(accepted_users)
 before = before[before.reviewerID.isin(accepted_users)]
 
-pivoted_after = after.pivot(index='reviewerID', columns='asin', values='overall').fillna(0)
+pivoted_after = after.pivot(index='reviewerID', columns='movieID', values='overall').fillna(0)
 
 #convert before dataframe to numpy array
-numpy_array = before.as_matrix(['reviewerID', 'asin', 'overall'])
+numpy_array = before.as_matrix(['reviewerID', 'movieID', 'overall'])
 
 #create product purchase matrix
 rows, row_pos = np.unique(numpy_array[:, 0], return_inverse=True)
@@ -78,17 +82,16 @@ cols, col_pos = np.unique(numpy_array[:, 1], return_inverse=True)
 pivot_table = np.zeros((len(rows), len(cols)), dtype=numpy_array.dtype)
 pivot_table[row_pos, col_pos] = numpy_array[:, 2]
 
-#calculate trust
-max_fre = product_data['count'].max()
-product_data['trust'] = product_data.apply (lambda row: calculate_trust (row, max_fre),axis=1)
+print product_data.describe();
+print len(product_data);
+number_of_sim_users = 5
 
-target = 22
-threshold = 5
+result = np.zeros((number_of_thresholds), dtype=np.float64);
 
 for target in range(len(accepted_users)):
 	print "target :", target
 	sim = np.array([calcuate_similarity(pivot_table, user_data, product_data, target, x) for x in range(len(accepted_users))])
-	sim_users = np.argpartition(sim, -threshold)[-threshold:]
+	sim_users = np.argpartition(sim, -number_of_sim_users)[-number_of_sim_users:]
 
 	purchase_count = {}
 
@@ -109,20 +112,46 @@ for target in range(len(accepted_users)):
 	for i in range(len(purchase_record)):
 		for u in sim_users:
 			if(pivot_table[u][purchase_record[i]]!=0):
-				recommend_prob[i] += sim[u]*(pivot_table[u][purchase_record[i]]-user_data.iloc[i, 0])
+				recommend_prob[i] += sim[u]*(pivot_table[u][purchase_record[i]]-user_data.iloc[u, 0])
 				weight_sum += sim[u]
 
 	for i in range(len(purchase_record)):
 		recommend_prob[i] = user_data.iloc[target, 0] + recommend_prob[i]/weight_sum[i]
 
-	count=0;
-	print "recommendation len :", len(np.where(recommend_prob>0)[0])
-	print "after puchase len :", len(after[after.reviewerID==rows[target]])
-	print "target id :", rows[target]
+	recommend_prob = np.true_divide(recommend_prob, 5)
 
-	for x in np.where(recommend_prob>0)[0]:
-		if(len(after[(after.reviewerID==rows[target]) & (after.asin==cols[purchase_record[x]])])>0):
-			count+=1
+	print recommend_prob.mean(), recommend_prob.min(), recommend_prob.max()
+	for t in range(number_of_thresholds):
+		threshold = getThreshold(t)
+		recommendation_list = np.where(recommend_prob>threshold)[0]
+		# print "after puchase len :", len(after[after.reviewerID==rows[target]])
+		# print "recommendation len :", len(recommendation_list)
+		# print "threshold :", threshold
 
-	print count
+		count=0;
+		for x in recommendation_list:
+			if(len(after[(after.reviewerID==rows[target]) & (after.movieID==cols[purchase_record[x]])])>0):
+				count+=1
+		if len(recommendation_list)>0 :
+			precision = count*1.0/len(recommendation_list)
+			result[t] += precision
+		# print "count :", count
+		# print "precision :", precision
+
+	if target>50:
+		break;
+
+result = np.true_divide(result, 50)
+
+f = open('collab.csv', 'w+')
+for i in range(number_of_thresholds):
+	s = str(getThreshold(i))+", "+str(result[i])+"\n"
+	f.write(s)
+f.close()
+
+plt.plot([getThreshold(i) for i in range(number_of_thresholds)], result)
+plt.axis([0.2, 0.6, 0, 0.6])
+plt.show()
+
+
 
--- movie_trust.py
+++ movie_trust.py
@@ -1,15 +1,18 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 import numpy as np
 import networkx as nx
 import json
 import math
 
+number_of_thresholds = 11
+
+def getThreshold(t):
+	return 0.20+(t*0.4/(number_of_thresholds-1))
+
 def calculate_trust(row, max_freq):
 	sigma = 0.5;
-	return sigma*row['mean']+((1-sigma)*row['count'])/max_freq
-
-def logic_function(x):
-	return 1/(1+math.exp(-x))
+	return (sigma*row['mean'])/5+((1-sigma)*row['count'])/max_freq
 
 def parse(path): 
 	f = open(path)
@@ -39,18 +42,21 @@ def calcuate_similarity(pivot_table, user_data, product_data, i, j):
 
 #create pandas dataframe
 
-df = getDF('Digital_Music_5.json')
+# df = pd.read_csv('ratings_Electronics.csv', header=None, names=['reviewerID', 'movieID', 'overall', 'unixReviewTime'], sep=',', engine='python')
+df = pd.read_csv('ml-1m/ratings.dat', header=None, names=['reviewerID', 'movieID', 'overall', 'unixReviewTime'], sep=':+', engine='python')
+# df = getDF('Digital_Music_5.json')
 #df = getDF('test_5500.json')
-df.drop(['reviewerName', 'helpful', 'reviewText', 'reviewTime', 'summary'], inplace=True, axis=1)
+# df.drop(['reviewerName', 'helpful', 'reviewText', 'reviewTime', 'summary'], inplace=True, axis=1)
 df.sort_values('unixReviewTime')
 
 #create product data
-product_data = pd.DataFrame(df.groupby('asin')['overall'].agg([np.mean, np.std, 'count'])).fillna(1)
+product_data = pd.DataFrame(df.groupby('movieID')['overall'].agg([np.mean, np.std, 'count'])).fillna(1)
 
-# top_product = product_data.sort_values('count').tail(20).index.values
-# df = df[df.asin.isin(top_product)]
+top_product = product_data.sort_values('count').tail(20).index.values
+df = df[df.movieID.isin(top_product)]
+print len(df)
 
-split_time = 1245000000
+split_time = df['unixReviewTime'].quantile([.75])[0.75]
 
 after = df[df.unixReviewTime>split_time]
 before = df[df.unixReviewTime<=split_time]
@@ -64,50 +70,42 @@ before = before[before.reviewerID.isin(common_users)]
 
 #create user data
 user_data = pd.DataFrame(before.groupby('reviewerID')['overall'].agg([np.mean, np.std, 'count'])).fillna(1)
+print "user_data"
 print user_data.describe();
 user_data = user_data[user_data['count'] > 4]
 accepted_users = user_data.index.values
 print len(accepted_users)
 before = before[before.reviewerID.isin(accepted_users)]
 
-pivoted_after = after.pivot(index='reviewerID', columns='asin', values='overall').fillna(0)
+pivoted_after = after.pivot(index='reviewerID', columns='movieID', values='overall').fillna(0)
 
 #convert before dataframe to numpy array
-numpy_array = before.as_matrix(['reviewerID', 'asin', 'overall'])
+numpy_array = before.as_matrix(['reviewerID', 'movieID', 'overall'])
 
 #create product purchase matrix
 rows, row_pos = np.unique(numpy_array[:, 0], return_inverse=True)
 cols, col_pos = np.unique(numpy_array[:, 1], return_inverse=True)
 
-print len(cols)
 pivot_table = np.zeros((len(rows), len(cols)), dtype=numpy_array.dtype)
 pivot_table[row_pos, col_pos] = numpy_array[:, 2]
-
-# corelation_matrix=np.zeros((20,20),dtype=np.int32 )
-# for group in df[df.asin.isin(top_product)].groupby('reviewerID'):
-# 	for x, row_x in group[1].iterrows():
-# 		for y, row_y in group[1].iterrows():
-# 			if x!=y:
-# 				a = np.where(top_product == row_x['asin'])[0][0]
-# 				b = np.where(top_product == row_y['asin'])[0][0]
-# 				corelation_matrix[a][b]+=1
-# 				corelation_matrix[b][a]+=1
-
-# print corelation_matrix
 
 #calculate trust
 max_fre = product_data['count'].max()
 product_data['trust'] = product_data.apply (lambda row: calculate_trust (row, max_fre),axis=1)
 
 print product_data.describe();
+print len(product_data);
 
-target = 22
-threshold = 5
+number_of_sim_users = 5
+
+result_precision = np.zeros((number_of_thresholds), dtype=np.float64);
+result_recall = np.zeros((number_of_thresholds), dtype=np.float64);
+result_f_score = np.zeros((number_of_thresholds), dtype=np.float64);
 
 for target in range(len(accepted_users)):
 	print "target :", target
 	sim = np.array([calcuate_similarity(pivot_table, user_data, product_data, target, x) for x in range(len(accepted_users))])	
-	sim_users = np.argpartition(sim, -threshold)[-threshold:]
+	sim_users = np.argpartition(sim, -number_of_sim_users)[-number_of_sim_users:]
 	sim_users = np.append(sim_users, [target])
 	#print sim_users
 
@@ -128,7 +126,7 @@ for target in range(len(accepted_users)):
 
 	for u in sim_users:
 		user_purchase = pd.DataFrame(before[before['reviewerID']==rows[u]].sort_values('unixReviewTime'))
-		purchase_transition = user_purchase['asin'].tolist()
+		purchase_transition = user_purchase['movieID'].tolist()
 		for i in range(len(purchase_transition)-1):
 			x = np.where(cols == purchase_transition[i])[0][0]
 			y = np.where(cols == purchase_transition[i+1])[0][0]
@@ -137,12 +135,12 @@ for target in range(len(accepted_users)):
 	for i in range(len(transition_matrix)):
 		transition_matrix[i] = transition_matrix[i]/purchase_count[purchase_record[i]]
 
-	for group in before[before['asin'].isin([cols[i] for i in purchase_record])].groupby('reviewerID'):
+	for group in before[before['movieID'].isin([cols[i] for i in purchase_record])].groupby('reviewerID'):
 		for x, row_x in group[1].sort_values('unixReviewTime').tail(3).iterrows():
 			for y, row_y in group[1].sort_values('unixReviewTime').tail(3).iterrows():
 				if x!=y:
-					a = np.where(cols == row_x['asin'])[0][0]
-					b = np.where(cols == row_y['asin'])[0][0]
+					a = np.where(cols == row_x['movieID'])[0][0]
+					b = np.where(cols == row_y['movieID'])[0][0]
 					corelation_matrix[purchase_record.index(a)][purchase_record.index(b)]+=1
 					corelation_matrix[purchase_record.index(b)][purchase_record.index(a)]+=1
 
@@ -151,7 +149,7 @@ for target in range(len(accepted_users)):
 	p = 0.7
 	transfer_matrix = p*transition_matrix + (1-p)*corelation_matrix;
 	last_three_purchase = before[before.reviewerID==rows[target]].tail(3)
-	recent_products = [purchase_record.index(np.where(cols == x)[0][0]) for x in last_three_purchase['asin'].tolist()]
+	recent_products = [purchase_record.index(np.where(cols == x)[0][0]) for x in last_three_purchase['movieID'].tolist()]
 
 	recommend_prob = np.zeros((len(purchase_record)), dtype=np.float64)
 
@@ -160,10 +158,51 @@ for target in range(len(accepted_users)):
 
 	recommend_prob = np.true_divide(recommend_prob, len(recent_products))
 
-	count=0;
-	for x in np.where(recommend_prob>0.0)[0]:
-		if(len(after[(after.reviewerID==rows[target]) & (after.asin==cols[purchase_record[x]])])>0):
-			count+=1
+	#include trust
+	alpha = 0.3
+	for x in range(len(purchase_record)):
+		recommend_prob[x] = alpha*recommend_prob[x] + (1 - alpha)*product_data.iloc[purchase_record[x], 3]
+	# print recommend_prob.mean(), recommend_prob.min(), recommend_prob.max()
+	for t in range(number_of_thresholds):
+		threshold = getThreshold(t)
+		
+		recommendation_list = np.where(recommend_prob>threshold)[0]
+		# print "after puchase len :", len(after[after.reviewerID==rows[target]])
+		# print "recommendation len :", len(recommendation_list)
+		# print "threshold :", threshold
 
-	print count
+		count=0;
+		after_purchased_count = len(after[after.reviewerID==rows[target]])
+		for x in recommendation_list:
+			if(len(after[(after.reviewerID==rows[target]) & (after.movieID==cols[purchase_record[x]])])>0):
+				count+=1
+		if len(recommendation_list)>0 :
+			precision = count*1.0/len(recommendation_list)
+			result_precision[t] += precision
+			recall = count*1.0/after_purchased_count
+			result_recall[t] += recall
+			if recall>1:
+				print "recall"
+			if((precision+recall)>0):
+				f = 2.0*precision*recall/(precision+recall)
+				result_f_score += f
+		# print "count :", count
+		# print "precision :", precision
+	print "recall :", result_recall
 
+	if target>=50:
+		break;
+
+result_precision = np.true_divide(result_precision, 51)
+result_recall = np.true_divide(result_recall, 51)
+result_f_score = np.true_divide(result_recall, 51)
+
+f = open('with_trust_3.csv', 'w+')
+for i in range(number_of_thresholds):
+	s = str(getThreshold(i))+", "+str(result_precision[i])+", "+str(result_recall[i])+", "+str(result_f_score[i])+"\n"
+	f.write(s)
+f.close()
+
+plt.plot([getThreshold(i) for i in range(number_of_thresholds)], result_precision)
+plt.axis([0.2, 0.6, 0, 0.6])
+plt.show()
